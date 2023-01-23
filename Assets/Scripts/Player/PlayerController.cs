105e9efa0db160974a219778670c66f28d7c6edf
JuicyBitch <116683349+JuicyBitch@users.noreply.github.com>
2023-01-23 16:33:38 -0500

    PlayerController fixed

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,56 +5,53 @@ using UnityEngine;
 public class PlayerController : SingletonMonoBehaviour<PlayerController>
 {
     [Header("Movement")]
-    private float moveSpeed;
-    public float walkSpeed;
-    public float sprintSpeed;
-
-    public float groundDrag;
-
-    public float jumpForce;
-    public float jumpCooldown;
-    public float airMultiplier;
-    bool readyToJump;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float sprintSpeed;
+    [SerializeField] private float groundDrag;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpCooldown;
+    [SerializeField] private float airMultiplier;
+    [SerializeField] private float maxSlopeAngle;
+    [SerializeField] private Transform orientation;
 
     [Header("Keybinds")]
-    public KeyCode jumpKey = KeyCode.Space;
-    public KeyCode sprintKey = KeyCode.LeftShift;
-
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+    
     [Header("Ground Check")]
-    public float playerHeight;
-    public LayerMask whatIsGround;
-    bool grounded;
-
-    [Header("Slope Handeling")]
-    public float maxSlopeAngle;
-    private RaycastHit slopeHit;
-    private bool exitingSlope;
-
-    public Transform orientation;
-
-    float horizontalInput;
-    float verticalInput;
-
-    Vector3 moveDirection;
-
-    Rigidbody rb;
-
-    public MovementState state;
+    [SerializeField] private float playerHeight;
+    [SerializeField] private LayerMask whatIsGround;
 
     [Header("Sound Gage")]
-    SoundGage soundGage;
     [SerializeField] GameObject soundGageDisplay;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private RaycastHit slopeHit;
+    private bool exitingSlope;
+    private float moveSpeed;
+    private Vector3 moveDirection;
+
+    private bool readyToJump;
+    private bool grounded;
+
+    private MovementState state;
+
+    private Rigidbody rb;
+    private SoundGage soundGage;
+
 
     public enum MovementState
     {
-        walking,
-        sprinting,
-        air
+        Walking,
+        Sprinting,
+        Air
     }
 
-    private void Start()
+    protected override void Awake()
     {
-        // Assigne et gèle la rotation du rigidbody
+        base.Awake();
+        // Assigne et gï¿½le la rotation du rigidbody
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         readyToJump = true;
@@ -69,10 +66,8 @@ public class PlayerController : SingletonMonoBehaviour<PlayerController>
         //Ground Check
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
 
-        MyInput();
-
+        Input();
         SpeedControl();
-
         StateHandler();
 
         //Handle drag;
@@ -92,18 +87,16 @@ public class PlayerController : SingletonMonoBehaviour<PlayerController>
         MovePlayer();
     }
 
-    private void MyInput()
+    private void Input()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
+        horizontalInput = UnityEngine.Input.GetAxisRaw("Horizontal");
+        verticalInput = UnityEngine.Input.GetAxisRaw("Vertical");
 
-        //Saute lorsque le joueur est prêt et au sol
-        if (Input.GetKey(jumpKey) && readyToJump && grounded)
+        //Saute lorsque le joueur est prï¿½t et au sol
+        if (UnityEngine.Input.GetKey(jumpKey) && readyToJump && grounded)
         {
             readyToJump = false;
-
             Jump();
-
             Invoke(nameof(ResetJump), jumpCooldown);
         }
     }
@@ -111,23 +104,23 @@ public class PlayerController : SingletonMonoBehaviour<PlayerController>
     private void StateHandler()
     {
         //Mode - sprinting
-        if(grounded && Input.GetKey(sprintKey))
+        if(grounded && UnityEngine.Input.GetKey(sprintKey))
         {
-            state = MovementState.sprinting;
+            state = MovementState.Sprinting;
             moveSpeed = sprintSpeed;
         }
 
         //Mode - walking
         else if (grounded)
         {
-            state = MovementState.walking;
+            state = MovementState.Walking;
             moveSpeed = walkSpeed;
         }
 
         //Mode - air
         else
         {
-            state = MovementState.air;
+            state = MovementState.Air;
         }
     }
 
@@ -140,13 +133,11 @@ public class PlayerController : SingletonMonoBehaviour<PlayerController>
         if (OnSlope() && !exitingSlope)
         {
             rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 20f, ForceMode.Force);
-
             // Permet de rester sur la pente (le joueur bondissait en montant une pente)
             if (rb.velocity.y > 0)
             {
                 rb.AddForce(Vector3.down * 80f, ForceMode.Force);
             }
-
         }
 
         // Sur le sol
@@ -154,7 +145,7 @@ public class PlayerController : SingletonMonoBehaviour<PlayerController>
         {
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
         }
-        if (grounded && verticalInput != 0 || horizontalInput != 0)
+        if(grounded && verticalInput != 0 || horizontalInput != 0)
         {
             soundGage.amplitude = 0.060f;
             soundGage.frequency = 10f;
@@ -174,29 +165,31 @@ public class PlayerController : SingletonMonoBehaviour<PlayerController>
             soundGage.frequency = 1f;
         }
 
-        // Pas de gravité sur les pentes
+        // Pas de gravitï¿½ sur les pentes
         rb.useGravity = !OnSlope();
     }
 
     private void SpeedControl()
     {
-        //Limite la vélocité sur les pentes
+        //Limite la vï¿½locitï¿½ sur les pentes
         if (OnSlope() && !exitingSlope)
         {
             if (rb.velocity.magnitude > moveSpeed)
+            {
                 rb.velocity = rb.velocity.normalized * moveSpeed;
+            }
         }
 
-        //Limite la vélocité sur le sol ou dans les airs
+        //Limite la vï¿½locitï¿½ sur le sol ou dans les airs
         else
         {
             Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
             if (flatVel.magnitude > moveSpeed)
             {
-                //Calcule la nouvelle vélocité si le moveSpeed dépasse celle préétablie
+                //Calcule la nouvelle vï¿½locitï¿½ si le moveSpeed dï¿½passe celle prï¿½ï¿½tablie
                 Vector3 limitedVel = flatVel.normalized * moveSpeed;
-                //Applique la nouvelle vélocité
+                //Applique la nouvelle vï¿½locitï¿½
                 rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
             }
         }
@@ -206,7 +199,7 @@ public class PlayerController : SingletonMonoBehaviour<PlayerController>
     {
         exitingSlope = true;
 
-        //Réinitialise la vélocité en y (le joueur saute toujours à la même hauteur)
+        //Rï¿½initialise la vï¿½locitï¿½ en y (le joueur saute toujours ï¿½ la mï¿½me hauteur)
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
         //Applique la force qu'une seule fois
@@ -216,7 +209,6 @@ public class PlayerController : SingletonMonoBehaviour<PlayerController>
     private void ResetJump()
     {
         readyToJump = true;
-
         exitingSlope = false;
     }
 
@@ -227,7 +219,6 @@ public class PlayerController : SingletonMonoBehaviour<PlayerController>
             float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
             return angle < maxSlopeAngle && angle != 0;
         }
-
         return false;
     }
 
