c7b2633891220e98b070d1ec6a739f6622778b92
JuicyBitch <116683349+JuicyBitch@users.noreply.github.com>
2023-02-10 20:08:55 -0500

    Player Tongue
    La grenouille projette sa langue en appuyant sur le bouton gauche de la souris.
    
    Il faut toutefois l'animer et compléter la fonction ExecuteTongue pour être en mesurer d'actionner les cordes/boutons et ramener les objets.

---
+++ Assets/Scripts/Player/PlayerTongue.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTongue : MonoBehaviour
+{
+    [Header("References")]
+
+    [SerializeField] private PlayerController pc;
+
+    public Transform cam;
+    public Transform tongueTip;
+    public LayerMask whatIsTongueable;
+    public LineRenderer lr;
+
+    [Header("Tongue")]
+
+    [SerializeField] private Vector3 tonguePoint;
+
+    public float maxTongueDistance;
+    public float tongueDelayTime;
+
+    [Header("Cooldown")]
+
+    [SerializeField] private float tongueCooldownTimer;
+
+    public float tongueCooldown;
+
+    [Header("Input")]
+
+    [SerializeField] bool isTonguing;
+
+    public KeyCode tongueKey = KeyCode.Mouse1;
+
+    private void Start()
+    {
+        pc = GetComponent<PlayerController>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(tongueKey)) StartTongue();
+
+        if (tongueCooldownTimer > 0) tongueCooldownTimer -= Time.deltaTime;
+    }
+
+    private void LateUpdate()
+    {
+        if (isTonguing) lr.SetPosition(0, tongueTip.position);
+    }
+
+    private void StartTongue()
+    {
+        if (tongueCooldownTimer > 0) return;
+
+        isTonguing = true;
+
+        RaycastHit hit;
+
+        //Si la langue touche
+        if (Physics.Raycast(cam.position, cam.forward, out hit, maxTongueDistance, whatIsTongueable))
+        {
+            tonguePoint = hit.point;
+
+            Invoke(nameof(ExecuteTongue), tongueDelayTime);
+        }
+        //Si la langue touche pas
+        else
+        {
+            tonguePoint = cam.position + cam.forward * maxTongueDistance;
+
+            Invoke(nameof(StopTongue), tongueDelayTime);
+        }
+
+        lr.enabled = true;
+        lr.SetPosition(1, tonguePoint);
+    }
+
+    private void ExecuteTongue()
+    {
+
+    }
+
+    private void StopTongue()
+    {
+        isTonguing = false;
+
+        tongueCooldownTimer = tongueCooldown;
+
+        lr.enabled = false;
+    }
+}
