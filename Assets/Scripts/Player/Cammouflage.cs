f91d486f7886c694d6abc9e621391d0d61c38f9f
Chestrade <anton.anyy@gmail.com>
2023-02-16 11:54:11 -0500

    Enemy doesn't detect frog when it's in cammo mode

--- Assets/Scripts/Player/Cammouflage.cs
+++ Assets/Scripts/Player/Cammouflage.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using IndieMarc.EnemyVision;
 
 public class Cammouflage : MonoBehaviour
 {
     public Material[] material;
     [SerializeField] private bool cammoPossible;
     [SerializeField] private GameObject frogChild;
+    [SerializeField] private VisionTarget vtScript;
     private Renderer rend;
     private PlayerController player;
+    
     
     // Start is called before the first frame update
     void Start()
@@ -35,10 +38,12 @@ public class Cammouflage : MonoBehaviour
         if(player.isIdle && player.grounded && cammoPossible)
         {
             rend.sharedMaterial = material[1];
+            vtScript.visible = false;
         }
         else
         {
             rend.sharedMaterial = material[0];
+            vtScript.visible = true;
         }
        
     }
