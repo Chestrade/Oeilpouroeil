6c1d486ec422012a8d375d6bc4ceefa35a2599d9
Chestrade <anton.anyy@gmail.com>
2023-03-07 21:34:04 -0500

    Adding cammo cooldown

--- Assets/Scripts/Player/Cammouflage.cs
+++ Assets/Scripts/Player/Cammouflage.cs
@@ -2,37 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using IndieMarc.EnemyVision;
+using UnityEngine.UI;
+using UnityEditor.Build;
 
 public class Cammouflage : MonoBehaviour
 {
+    [Header("Cammo Settings")]
+    public bool isCammouflaged;
+    [SerializeField] private bool cammoAcquired;
+    
+    
+
+    [Header ("Cammo Cooldown")]
+    [SerializeField] private Slider cooldownSlider;
+    [SerializeField] private int cooldownTime;
+    private int currentTime;
+    private WaitForSeconds regenTick = new WaitForSeconds(0.1f);
 
     [Header("Materials and Refs")]
     public Material[] material;
-    [SerializeField] private bool cammoPossible;
     [SerializeField] private GameObject frogChild;
     [SerializeField] private VisionTarget vtScript;
     [SerializeField] private MeshCollider playerColl;
     [SerializeField] protected ParticleSystem enterCammoParticles;
     [SerializeField] protected ParticleSystem exitCammoParticles;
+   
 
     [Header("Wwise Events")]
     public AK.Wwise.Event cammoEnter;
     public AK.Wwise.Event cammoExit;
 
-    public bool isCammouflaged;
+   
 
     private Renderer rend;
     private PlayerController player;
     private CapsuleCollider enemyColl;
     private bool wwiseEventPlayed;
-
+    private float lastCammo;
     //particle system variables
-    
-    
 
 
 
-    
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -40,12 +53,15 @@ public class Cammouflage : MonoBehaviour
         rend = frogChild.GetComponent<Renderer>();
         rend.enabled = true;
         rend.sharedMaterial = material[0];
-        cammoPossible = false;
+        cammoAcquired = false;
         wwiseEventPlayed = false;
         isCammouflaged = false;
-        
-        
-        
+
+        //Cooldown Initiation
+        currentTime = cooldownTime;
+        cooldownSlider.maxValue = cooldownTime;
+        cooldownSlider.value = cooldownTime;
+
         enemyColl = GameObject.Find("Enemy/Mesh").GetComponent<CapsuleCollider>();
 
     }
@@ -55,38 +71,37 @@ public class Cammouflage : MonoBehaviour
     {
         ToggleCammo();
         UseCammo();
+
+       
     }
 
     public void ToggleCammo()
     {
         //changer le code ici pour que cammouflage s'active avec la statue
-        if (Input.GetKeyDown(KeyCode.C) && cammoPossible == false)
+        if (Input.GetKeyDown(KeyCode.C) && cammoAcquired == false)
         {
-            cammoPossible = true;
+            cammoAcquired = true;
         }
-        else if (Input.GetKeyDown(KeyCode.C) && cammoPossible == true)
+        else if (Input.GetKeyDown(KeyCode.C) && cammoAcquired == true)
         {
-            cammoPossible = false;
+            cammoAcquired = false;
         }
     }
 
     void UseCammo()
     {
-        if (player.isIdle && player.grounded && cammoPossible)
+        if (player.isIdle && player.grounded && cammoAcquired && currentTime >= cooldownTime)
         {
             isCammouflaged = true;
             Physics.IgnoreCollision(playerColl, enemyColl, true);
             rend.sharedMaterial = material[1];
             vtScript.visible = false;
-            
-           
-
+            depleteCammo(cooldownTime);
 
             if (wwiseEventPlayed == false)
             {
                 cammoEnter.Post(gameObject);
                 enterCammoParticles.Play();
-                //Debug.Log("The frog is cammouflaged");
                 wwiseEventPlayed = true;
             }
 
@@ -102,13 +117,33 @@ public class Cammouflage : MonoBehaviour
             {
                 cammoExit.Post(gameObject);
                 exitCammoParticles.Play();
-                //Debug.Log("The frog is not cammouflaged");
                 wwiseEventPlayed = false;
+
+                //trigger cooldown
+                StartCoroutine(ResetCooldown());
             }
-
-        
-
         }
     }
-    
+    void depleteCammo(int amount)
+    {
+        if (currentTime - amount >= 0)
+        {
+            currentTime -= amount;
+            cooldownSlider.value = currentTime; 
+        }
+        else
+        {
+            return;
+        }
+    }
+    private IEnumerator ResetCooldown()
+    {
+        yield return null;
+        while(currentTime < cooldownTime)
+        {
+            currentTime += cooldownTime / 100;
+            cooldownSlider.value = currentTime;
+            yield return regenTick;
+        }
+    }
 }
